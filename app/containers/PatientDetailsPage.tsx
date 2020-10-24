9ffdfca1ded7695e23a7c1cf89619cc83334a7a6
Divyansh <divyansh.yadav@gmail.com>
2020-10-24 16:14:44 +0530

    add formic

--- app/containers/PatientDetailsPage.tsx
+++ app/containers/PatientDetailsPage.tsx
@@ -1,13 +1,72 @@
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Formik } from 'formik';
 import BackButton from '../components/BackButton';
 
+interface Patient {
+  email: string;
+}
+
+interface Errors {
+  email?: string;
+}
+
 export default function PatientDetailsPage() {
+  const initialValues: Patient = { email: '' };
   return (
     <div>
       <BackButton />
-      <h1>Patient form</h1>
-      <Button color="danger">Danger!</Button>
+      <h1>Patient details</h1>
+      <Formik
+        initialValues={initialValues}
+        validate={(values) => {
+          const errors: Errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <Form onSubmit={handleSubmit} novalidate>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="with a placeholder"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              <div>{errors.email && touched.email && errors.email}</div>
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </FormGroup>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
